28724cdb1263386e32057897889faeaeda96b145
gloryakinyi <akinyiglory2@gmail.com>
2025-04-09 08:20:42 +0300

    Initial commit

---
+++ app/src/main/java/com/glory/scaffold/navigation/AppNavHost.kt
@@ -0,0 +1,34 @@
+package com.glory.scaffold.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.glory.scaffold.ui.screens.about.AboutScreen
+import com.glory.scaffold.ui.screens.home.HomeScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_HOME
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_HOME) {
+            HomeScreen(navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController)
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/glory/scaffold/navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.glory.scaffold.navigation
+
+
+const val ROUT_HOME = "home"
+const val ROUT_ABOUT = "about"
